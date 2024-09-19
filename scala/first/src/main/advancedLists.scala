4ca54fafb5c64b01aace9b07c7fe3357316eadea
Walidg <walid.gafsi@gmail.com>
2024-09-19 08:47:47 +0200

    Add advanced lists merge sort method

--- scala/first/src/main/advancedLists.scala
+++ scala/first/src/main/advancedLists.scala
@@ -5,17 +5,32 @@ extension [T] (e : List[T])
     def removeAt(n : Int) : List[T] = e match
         case Nil => Nil
         case x :: xs => if n == 0 then xs else x :: xs.removeAt(n-1) 
+
     def insertionSort : List[T] = e match
         case Nil => Nil
         case x :: xs => insert(x, xs.insertionSort)
 
+    def mergeSort : List[T] = 
+        val n = e.length / 2
+        if n == 0 then e
+        else 
+            def merge(l : List[T], r : List[T]) : List[T] = (l, r) match
+                case (Nil, ys) => ys
+                case (xs, Nil) => xs 
+                case (x :: xs, y :: ys) => if lessThan(x, y) then x :: merge(xs, r) else y :: merge(l, ys)
+            
+            val (left, right) = e.splitAt(n) 
+            merge(left.mergeSort, right.mergeSort)
+
 private def insert[T]( i : T, xs : List[T]) : List[T] = xs match
     case Nil => List(i)
-    case y :: ys => if compareTo(i, y) then i :: xs else y :: insert(i, ys)
-private def compareTo(a1 : Any, a2 : Any) : Boolean = (a1, a2) match
+    case y :: ys => if lessThan(i, y) then i :: xs else y :: insert(i, ys)
+private def lessThan(a1 : Any, a2 : Any) : Boolean = (a1, a2) match
     case (z1 : Int, z2 : Int) => z1 < z2
     case (z1 : Double, z2 : Double) => z1 < z2
     case (z1 : Char, z2 : Char) => z1 < z2
+    case (z1 : Boolean, z2 : Boolean) => !z1 && z2
+    case (z1 : String, z2 : String) => z1.compareTo(z2) < 0
     case _ => ???
     
 
@@ -48,3 +63,16 @@ def flatten(xs : List[Any]) : List[Any] = xs match
     assert(List(5.0, 4.2, 7, 2, 3, 9, 6, 1, 5).insertionSort == List(1, 2, 3 ,4.2 , 5, 5, 6, 7, 9))
 
     assert("feairpji".toList.insertionSort == "aefiijpr".toList)
+
+    assert(List("berries", "apples", "pears", "oranges", "peaches").insertionSort == 
+        List("apples", "berries", "oranges", "peaches", "pears"))
+
+
+    assert(List(5, 4, 7, 2, 3, 9, 6, 1, 5).mergeSort == List(1, 2, 3 ,4 , 5, 5, 6, 7, 9))
+
+    assert(List(5.0, 4.2, 7, 2, 3, 9, 6, 1, 5).mergeSort == List(1, 2, 3 ,4.2 , 5, 5, 6, 7, 9))
+
+    assert("feairpji".toList.mergeSort == "aefiijpr".toList)
+
+    assert(List("berries", "apples", "pears", "oranges", "peaches").mergeSort == 
+        List("apples", "berries", "oranges", "peaches", "pears"))
