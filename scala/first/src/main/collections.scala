484441981e67e4c934155ee39db43fc6c6882cd6
Walidg <walid.gafsi@gmail.com>
2024-09-22 22:38:50 +0200

    Add Polynom class

--- scala/first/src/main/collections.scala
+++ scala/first/src/main/collections.scala
@@ -33,9 +33,60 @@ def primePairs2(n : Int) : Seq[(Int, Int)] =
         if isPrime(i +j)
     yield (i, j)
 
+
+class Polynom(nonZeroElements : Map[Int, Double]) :
+    def this(bindings : (Int, Double)* ) = this(bindings.toMap)
+
+    val terms = nonZeroElements.withDefaultValue(0.0)
+
+    def +(other: Polynom) : Polynom = Polynom(terms ++ other.terms.map((exp, coeff) => (exp, terms(exp) + coeff)))
+
+    def *(other: Polynom) : Polynom = 
+        (for (exp, coeff) <- other.terms
+        yield Polynom(terms.map((x, y) => (x + exp, y * coeff))))
+        .reduce(_ + _)
+    
+    def *(other: Double) : Polynom = Polynom(terms.map((exp, coeff) => (exp, coeff * other)))
+
+    override def toString(): String = 
+        val termsStrings = 
+            for (exp, coeff) <- terms.toList.sorted.reverse
+            yield 
+                val exponent = exp match
+                    case 0 => ""
+                    case 1 => "x"
+                    case _ => s"x^$exp"
+                s"$coeff$exponent"
+        if terms.isEmpty then "0"
+        else termsStrings.init.foldRight(s"${termsStrings.last}") ((x, acc) => acc.toList match
+            case '-' :: xs => s"$x - ${acc.substring(1)}" 
+            case _ => s"$x + $acc"
+            )
+
+    override def equals(x: Any): Boolean = x match
+        case p : Polynom => checkEquals(p)
+        case _ => false
+
+    private def checkEquals(other : Polynom) : Boolean = 
+        terms.size == other.terms.size 
+        && terms.toList.sorted.zip(other.terms.toList.sorted).forall((kv1, kv2) => kv1._1 == kv2._1 && kv1._2 == kv2._2)
+    
+
+extension (e : Double) 
+    def *(p : Polynom) = p * e
+
+
 @main def test =
     assert(scalarProduct(Vector(1, 2, 3), Vector(5, 6, 7)) == 38)
     assert(isPrime(53))
     assert(isPrime(2))
     assert(primePairs2(7) == primePairs1((7)))
     assert(scalarProduct(Vector(1, 2, 3), Vector(5, 6, 7)) == scalarProduct2(Vector(1, 2, 3), Vector(5, 6, 7)))
+
+    val p1 = Polynom(0 -> 5, 1 -> -2, 3 -> -2)
+    val p2 = Polynom(1 -> 3, 2 -> 3)
+    val product = Polynom(5 -> -6, 1 -> 15, 2 -> 9, 3 -> -6, 4 -> -6)
+
+    assert(product == p1 * p2)
+    assert(product != p1)
+    assert(2 * p2 == p2 * 2)
